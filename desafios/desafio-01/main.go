69a936287921f430e61de7bfee89792bbe7d67bb
Felipe Amaral <amaral.felipe@live.com>
2021-07-12 11:42:21 -0300

    Criaçao dos modulos para preparar e publicar as mensagens no Kafka

--- desafios/desafio-01/main.go
+++ desafios/desafio-01/main.go
@@ -11,6 +11,6 @@ func main() {
 	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
 
-	log.Println("Executando na porta 3000...")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Println("Executando na porta 8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
